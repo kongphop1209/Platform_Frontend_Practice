1c7c31504cd9c9548a9d0dcfe752b537a199f5b7
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-22 20:24:53 +0700

    a

---
+++ fronttest/src/app/components/board.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const board = () => {
+  return (
+    <div className='flex justify-start w-1/2 h-1/2 bg-white'>
+        <div className='h-12 w-screen bg-blue-200 flex justify-start items-center'>
+            <p className='ms-5'>User Management</p>
+        </div>
+    </div>
+  )
+}
+
+export default board
