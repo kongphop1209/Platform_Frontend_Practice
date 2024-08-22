25d745ff8e53b522f2683dd672854eb2966709ba
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-22 20:38:03 +0700

    Update board.tsx

--- fronttest/src/app/components/board.tsx
+++ fronttest/src/app/components/board.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const board = () => {
   return (
-    <div className='flex justify-start w-1/2 h-1/2 bg-white'>
-        <div className='h-12 w-screen bg-blue-200 flex justify-start items-center'>
+    <div className='flex justify-start w-1/2 h-1/2 bg-white border-neutral-700 border-y border-x rounded-lg'>
+        <div className='h-12 w-full bg-blue-200 flex justify-start items-center rounded-t-lg'>
             <p className='ms-5'>User Management</p>
         </div>
     </div>
