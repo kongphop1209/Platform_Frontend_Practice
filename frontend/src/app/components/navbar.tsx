d93c116e28f352a4c8227adb4942f6d544b00ac9
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-23 15:42:07 +0700

    Update navbar.tsx

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -6,8 +6,8 @@ const navbar = () => {
     <div className="flex-1 h-screen bg-blue-100">
       <div className="flex justify-center items-center bg-red-500 h-12">
         <h1 className=" text-white size-10 w-auto h-auto cursor-pointer">
-          <Link href="/">
-            <a>Sport Complex</a>
+          <Link href="#">
+            Sport Complex
           </Link>
         </h1>
       </div>
