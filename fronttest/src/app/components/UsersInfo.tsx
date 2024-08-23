9cc6823b5a1fc2935b3cc5a9e6a65a06397d9b50
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-23 17:21:25 +0700

    Update UsersInfo.tsx

--- fronttest/src/app/components/UsersInfo.tsx
+++ fronttest/src/app/components/UsersInfo.tsx
@@ -5,11 +5,11 @@ const UsersInfo = () => {
     <div className='w-full mt-4'>
         <div className='bg-blue-400 mx-5 py-2'>
           <span>
-            <ul className='flex flex-row'>
-              <li>ID</li>
-              <li>Username</li>
-              <li>Name</li>
-              <li>Email</li>
+            <ul className='flex flex-row mx-8'>
+              <li className='flex-none w-1/12 flex justify-start'>ID</li>
+              <li className='flex-none w-1/4 flex justify-start'>Username</li>
+              <li className='flex-none w-1/4 flex justify-start'>Name</li>
+              <li className='flex-none w-1/3 flex justify-start'>Email</li>
             </ul>
           </span>
         </div>
