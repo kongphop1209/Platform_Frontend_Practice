3973d0a0b8d0cd6c4fd7dff3e38953f4a3265ac1
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-23 10:18:26 +0700

    Update navbar.tsx

--- frontend/src/app/components/navbar.tsx
+++ frontend/src/app/components/navbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const navbar = () => {
   return (
     <div className="flex-1 h-screen bg-blue-100">
       <div className="flex justify-center items-center bg-red-500 h-12">
         <h1 className=" text-white size-10 w-auto h-auto cursor-pointer">
-          Sport Complex
+          <Link href="/">
+            <a>Sport Complex</a>
+          </Link>
         </h1>
       </div>
     </div>
