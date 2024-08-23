e24912d842525d906a696599b02718e3bf310a52
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-23 15:58:18 +0700

    a

--- fronttest/src/app/components/board.tsx
+++ fronttest/src/app/components/board.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SearchBar from './searchbar'
 
 const board = () => {
   return (
-    <div className='flex justify-start w-1/2 h-1/2 bg-white border-neutral-700 border-y border-x rounded-lg'>
-        <div className='h-12 w-full bg-blue-200 flex justify-start items-center rounded-t-lg'>
-            <p className='ms-5'>User Management</p>
+    
+    <div className='flex flex-col justify-start w-1/2 h-1/2 bg-white border-blue-200 border-y border-x rounded-lg'>
+        <div className='h-12 w-full bg-blue-400 flex justify-start items-center rounded-t-md'>
+            <p className='ms-5 inline-flex flex-row items-center gap-2 text-white'>
+              <FontAwesomeIcon icon={faUser} className='size-5' />
+              User Management
+            </p>
         </div>
+        <SearchBar/>
     </div>
   )
 }
--- fronttest/src/app/page.tsx
+++ fronttest/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Board from './components/board'
 
+
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen">
