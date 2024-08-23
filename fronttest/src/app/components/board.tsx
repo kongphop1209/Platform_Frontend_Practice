9e559001e9585d264efa4d6f6d1bc02889e20131
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-08-23 16:49:28 +0700

    a

--- fronttest/src/app/components/board.tsx
+++ fronttest/src/app/components/board.tsx
@@ -3,10 +3,10 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SearchBar from './searchbar'
 
-const board = () => {
+const board: React.FC = () => {
   return (
     
-    <div className='flex flex-col justify-start w-1/2 h-1/2 bg-white border-blue-200 border-y border-x rounded-lg'>
+    <div className='flex flex-col justify-start w-1/2 h-1/2 bg-slate-200 border-blue-200 border-y border-x rounded-lg'>
         <div className='h-12 w-full bg-blue-400 flex justify-start items-center rounded-t-md'>
             <p className='ms-5 inline-flex flex-row items-center gap-2 text-white'>
               <FontAwesomeIcon icon={faUser} className='size-5' />
--- fronttest/src/app/components/searchbar.tsx
+++ fronttest/src/app/components/searchbar.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const searchbar = () => {
+const searchbar: React.FC = () => {
+  const [query, setQuery] = useState<string>("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Searching for:", query);
+    // Add your search handling logic here
+  };
+
   return (
-    <div>searchbar</div>
-  )
-}
+    <form onSubmit={handleSearch} className="flex items-center justify-center mt-4">
+      <div className="relative w-2/3 max-w-lg">
+        <input
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          placeholder="Search users..."
+          className="w-full p-3 pl-10 text-sm border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 right-0 px-4 py-2 text-sm font-medium text-white bg-blue-400 rounded-r-lg hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default searchbar
+export default searchbar;
